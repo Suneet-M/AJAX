16ceacaddddc1221385770f37c5c1c73d6046e43
Suneet-M <43340251+Suneet-M@users.noreply.github.com>
2018-12-08 15:45:03 +0530

    feat: Use Fetch API for fetching image from unsplash

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,27 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        fetch(`https://api.unsplash.com/search/photos?query="${searchedForText}"`, {
+            headers: {
+                Authorization: 'Client-ID 265cbd43910e6142fdd0845465c9746e5d4295d3a0bed326edbca6585c3e06de'
+            }
+        }).then(response => response.json())
+            .then(response => {
+                let htmlContent;
+                if (response && response.results && response.results[0]) {
+                    const firstImage = response .results[0];
+                    htmlContent = `<figure>
+                        <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                        </figure>`;
+                } else {
+                    htmlContent = 'Sorry, no image found!';
+                }
+                responseContainer.innerHTML = htmlContent;
+            })
+            .catch(error => {
+                throw Error(error);
+            });
     });
 })();
