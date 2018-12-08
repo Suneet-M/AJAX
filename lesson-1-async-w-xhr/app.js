317213dc1e8fd824ab91af8839a6f7ef398141a2
Suneet-M <43340251+Suneet-M@users.noreply.github.com>
2018-12-08 12:24:03 +0530

    feat: Create XHR for images from unsplash

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -4,9 +4,21 @@
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
+    function addImage() {}
+
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?query=${searchedForText}`);
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 265cbd43910e6142fdd0845465c9746e5d4295d3a0bed326edbca6585c3e06de');
+        unsplashRequest.onload = addImage;
+        unsplashRequest.onerror = imageError;
+        function imageError() {
+            console.log('Search request error. Check network connection.');
+        }
+        unsplashRequest.send();
     });
 })();
